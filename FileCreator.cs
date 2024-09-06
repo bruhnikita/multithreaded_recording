bce4ce30272935e02a71b043b55fcc887f3665ff
bruhnikita <niksmailgood@mail.ru>
2024-09-06 23:18:57 +0700

    first

---
+++ FileCreator.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multithreaded_recording
+{
+    public class FileCreator
+    {
+        string path;
+
+        public FileCreator()
+        {
+            path = "log.txt";
+            FileChecking();
+        }
+
+        private void FileChecking()
+        {
+            if (File.Exists(path)) FileCreating();
+        }
+
+        private void FileCreating()
+        {
+            File.Create(path);
+        }
+
+        public void WritingToFile()
+        {
+            string message = "Поток 1: " + DateTime.Now + "\n";
+            byte[] info = new UTF8Encoding(true).GetBytes(message);
+
+            try
+            {
+                using (FileStream fs = File.Open(path, FileMode.Append, FileAccess.Write))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        fs.Write(info, 0, info.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка ввода в файл: " + ex.Message);
+            }
+        }
+
+        public void ReadingFile()
+        {
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string line = "";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка чтения: " + ex.Message);
+            }
+        }
+        public void MultithreadedRecording()
+        {
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    using (FileStream fs = File.Open(path, FileMode.Append, FileAccess.Write))
+                    {
+                        byte[] info = new UTF8Encoding(true).GetBytes("Поток 1: " + DateTime.Now + "\n");
+                        for (int j = 0; j < 5; j++)
+                            fs.Write(info, 0, info.Length);
+                        info = new UTF8Encoding(true).GetBytes("Поток 2: " + DateTime.Now + "\n");
+                        for (int j = 0; j < 5; j++)
+                            fs.Write(info, 0, info.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка многопоточной записи: " + ex.Message);
+            }
+        }
+
+        public void DeleteFile()
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+
+                    Console.WriteLine("Файл успешно удален.");
+                }
+
+                else 
+                    Console.WriteLine("Файл не найден.");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Ошибка удаления файла: " + ex.Message);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿namespace multithreaded_recording
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            FileCreator creator = new FileCreator();
+
+            while (true)
+            {
+                Console.WriteLine("Введите действие:\n1 - Запись в файл\n2 - Чтение из файла\n3 - параллельная запись\n4 - удалить файл\n5 - выход");
+
+                char choice = Console.ReadKey().KeyChar;
+
+                Console.Clear();
+
+                switch (choice)
+                {
+                    case '1':
+                        creator.WritingToFile();
+                        break;
+
+                    case '2':
+                        creator.ReadingFile();
+                        Console.WriteLine();
+                        break;
+
+                    case '3':
+                        creator.MultithreadedRecording();
+                        break;
+
+                    case '4':
+                        creator.DeleteFile();
+                        break;
+
+                    case '5':
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+    }
+}
